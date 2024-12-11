42dba71759a56a9462c10e4365803364c3c2847c
Shaokun Wu <shaokun.wu@gmail.com>
2024-12-11 11:29:28 +0800

    Add UploadImages, add Installation section to README.md

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .upload_images import UploadImages
+
+NODE_CLASS_MAPPINGS = {
+    "UploadImagesToInstantStudio": UploadImages
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "UploadImagesToInstantStudio": "Upload Images to Instant Studio"
+}
